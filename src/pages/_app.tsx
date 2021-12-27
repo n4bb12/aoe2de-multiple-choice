17eddd49018cac5fdfcf1ca276c9fc66a922c284
Abraham Schilling <AbrahamSchilling@gmail.com>
2021-12-28 00:41:28 +0100

    Add app icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app"
 import React, { FC } from "react"
 import { Background } from "src/components/Background"
 import { Footer } from "src/components/Footer"
+import { Head } from "src/components/Head"
 import { Header } from "src/components/Header"
 import { Main } from "src/components/Main"
 import "src/global.css"
@@ -11,15 +12,18 @@ const CustomApp: FC<AppProps> = ({ Component, pageProps }) => {
   useWakeLock()
 
   return (
-    <Background>
-      <div className="grid items-start w-screen min-h-screen grid-rows-[auto,1fr,auto] gap-4 sm:gap-8 p-4 sm:p-8 leading-tight">
-        <Header />
-        <Main>
-          <Component {...pageProps} />
-        </Main>
-        <Footer />
-      </div>
-    </Background>
+    <>
+      <Head />
+      <Background>
+        <div className="grid items-start w-screen min-h-screen grid-rows-[auto,1fr,auto] gap-4 sm:gap-8 p-4 sm:p-8 leading-tight">
+          <Header />
+          <Main>
+            <Component {...pageProps} />
+          </Main>
+          <Footer />
+        </div>
+      </Background>
+    </>
   )
 }
 
