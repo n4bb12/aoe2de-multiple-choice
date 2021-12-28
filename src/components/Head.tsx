6c35749234ba558718640e1e8db8d8e6d18adb5c
Abraham Schilling <AbrahamSchilling@gmail.com>
2021-12-28 01:35:24 +0100

    Update meta theme color

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -15,7 +15,7 @@ export const Head: FC<HeadProps> = ({ title }) => (
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <link rel="manifest" href="/site.webmanifest" />
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#181c29" />
-    <meta name="msapplication-TileColor" content="#ffdf91" />
-    <meta name="theme-color" content="#ffdf91"></meta>
+    <meta name="msapplication-TileColor" content="#181c29" />
+    <meta name="theme-color" content="#181c29"></meta>
   </NextHead>
 )
