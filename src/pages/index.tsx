740bd696920b17b03acf345b3d82a9748bda3c13
Abraham Schilling <AbrahamSchilling@gmail.com>
2021-12-28 10:09:01 +0100

    Add page title to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,50 @@
 import React, { FC } from "react"
+import { AProps } from "src/components/A"
 import { Button } from "src/components/Button"
+import { Head } from "src/components/Head"
 
-const LeftButton: typeof Button = ({ children, ...props }) => (
-  <Button {...props}>
+const LeftLink: FC<AProps> = ({ children, ...props }) => (
+  <Button {...props} as="link">
     <div className="px-3 text-right">{children}</div>
   </Button>
 )
 
-const RightButton: typeof Button = ({ children, ...props }) => (
-  <Button {...props}>
+const RightLink: FC<AProps> = ({ children, ...props }) => (
+  <Button {...props} as="link">
     <div className="px-3 text-left">{children}</div>
   </Button>
 )
 
+const Links: FC = () => (
+  <div className="grid grid-cols-2 gap-2">
+    <LeftLink href="/wappen-volk">Wappen &#10146; Volk</LeftLink>
+    <RightLink href="/volk-wappen">Volk &#10146; Wappen</RightLink>
+
+    <LeftLink href="/spezialeinheit-volk">
+      Spezialeinheit &#10146; Volk
+    </LeftLink>
+    <RightLink href="/volk-spezialeinheit">
+      Volk &#10146; Spezialeinheit
+    </RightLink>
+
+    <LeftLink href="/bonus-volk">Bonus &#10146; Volk</LeftLink>
+    <RightLink href="/volk-bonus">Volk &#10146; Bonus</RightLink>
+
+    <LeftLink href="/team-bonus-volk">Team-Bonus &#10146; Volk</LeftLink>
+    <RightLink href="/volk-team-bonus">Volk &#10146; Team-Bonus</RightLink>
+
+    <Button href="/alle-fragen" className="col-span-2">
+      Alle Fragen
+    </Button>
+  </div>
+)
+
 const Page: FC = () => {
   return (
-    <div className="grid grid-cols-2 gap-2">
-      <LeftButton as="link" href="/wappen-volk">
-        Wappen &#10146; Volk
-      </LeftButton>
-      <RightButton as="link" href="/volk-wappen">
-        Volk &#10146; Wappen
-      </RightButton>
-
-      <LeftButton as="link" href="/spezialeinheit-volk">
-        Spezialeinheit &#10146; Volk
-      </LeftButton>
-      <RightButton as="link" href="/volk-spezialeinheit">
-        Volk &#10146; Spezialeinheit
-      </RightButton>
-
-      <LeftButton as="link" href="/bonus-volk">
-        Bonus &#10146; Volk
-      </LeftButton>
-      <RightButton as="link" href="/volk-bonus">
-        Volk &#10146; Bonus
-      </RightButton>
-
-      <LeftButton as="link" href="/team-bonus-volk">
-        Team-Bonus &#10146; Volk
-      </LeftButton>
-      <RightButton as="link" href="/volk-team-bonus">
-        Volk &#10146; Team-Bonus
-      </RightButton>
-
-      <Button as="link" href="/alle-fragen" className="col-span-2">
-        Alle Fragen
-      </Button>
-    </div>
+    <>
+      <Head title="AOE2DE Fragen" />
+      <Links />
+    </>
   )
 }
 
