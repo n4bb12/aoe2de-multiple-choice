fedcb7104233d8ffcb6905ddf026ffca834ad563
Abraham Schilling <AbrahamSchilling@gmail.com>
2021-12-28 01:29:46 +0100

    Update header on home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,28 +3,36 @@ import React, { FC } from "react"
 import { A } from "./A"
 import { Card } from "./Card"
 
-export const Header: FC = () => {
+const Content: FC = () => {
   const router = useRouter()
 
   if (router.asPath === "/") {
-    return <header />
+    return <>AOE2DE Fragen</>
   }
 
   return (
+    <nav>
+      <ul>
+        <li>
+          <A
+            href="/"
+            className="fine:hover:underline underline-offset-2 decoration-blue-700"
+          >
+            Startseite
+          </A>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export const Header: FC = () => {
+  return (
     <header className="row-start-2 sm:row-start-1">
       <Card>
-        <nav className="p-4">
-          <ul className="text-center">
-            <li>
-              <A
-                href="/"
-                className="fine:hover:underline underline-offset-2 decoration-blue-700"
-              >
-                Startseite
-              </A>
-            </li>
-          </ul>
-        </nav>
+        <div className="p-4 text-center">
+          <Content />
+        </div>
       </Card>
     </header>
   )
